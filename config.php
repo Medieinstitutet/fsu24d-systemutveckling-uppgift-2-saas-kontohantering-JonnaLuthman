dd1628b497d44d8039ca7fa3ea1a78d8c4fa90df
Jonna Luthman <jonna.luthman@medieinstitutet.se>
2025-05-12 16:02:57 +0100

    - Added dockerfile and configuration. - Added pages, without content.

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+
+function fsu24d_test() {
+    echo("fsu24d_test");
+}
+
+define('DB_NAME', 'db');
+define('DB_HOST', 'db');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'EstherTheDog');
+?>
+
+<?php
+// function fsu24d_test() {
+//     echo("fsu24d_test");
+// }
+
+//     define('DB_NAME', 'products');
+//     define('DB_HOST', 'db');
+//     define('DB_USER', 'root');
+//     define('DB_PASSWORD', 'notSecureChangeMe');
+
+//     $connection = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+// ?>
