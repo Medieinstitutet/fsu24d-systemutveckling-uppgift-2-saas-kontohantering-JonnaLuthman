79e98632500b008140ab0a720953c9ae88f86766
Jonna Luthman <jonna.luthman@medieinstitutet.se>
2025-05-20 10:56:30 +0100

    Implement newsletter subscription and unsubscription functionality

--- html/private/includes/newsletter_functions.php
+++ html/private/includes/newsletter_functions.php
@@ -15,6 +15,20 @@ function get_newsletters() {
     }
 }
 
+function get_newsletter($id) {
+    global $connection;
+
+    $sql = "SELECT * FROM newsletters WHERE id='$id'";
+
+    $result = $connection->query($sql);
+    
+    if ($result && $result->num_rows > 0) {
+    return $result->fetch_array(MYSQLI_ASSOC);
+    } else {
+    return [];
+    }
+}
+
 function get_user_subscriptions($user_id) {
     global $connection;
 
@@ -42,6 +56,41 @@ function create_newsletter($user_email, $title = '', $description = '') {
         return true;
     } catch(exception $error) {
          return $error ? ['error' => $error] : ['message' => $result];
+    }
+}
+
+function user_is_subscribed($user_id, $newsletter_id) {
+    global $connection;
+
+    $query = ("SELECT * FROM `subscriptions` WHERE `user_id` = '$user_id' AND `newsletter_id` = '$newsletter_id'");
+    $result = $connection->query($query);
+
+    return $result && $result->num_rows > 0;
+}
+
+function subscribe_to_newsletter($user_id, $newsletter_id){
+    global $connection;
+
+    $query = "INSERT INTO `subscriptions`(`user_id`, `newsletter_id`) VALUES ('$user_id','$newsletter_id')";
+    $result = $connection->query($query);
+
+    if($result) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function unsubscribe_to_newsletter($user_id, $newsletter_id){
+    global $connection;
+
+    $query = "DELETE FROM `subscriptions` WHERE `subscriptions`.`user_id`='$user_id' and `subscriptions`.`newsletter_id` = '$newsletter_id';";
+    $result = $connection->query($query);
+
+    if($result) {
+        return true;
+    } else {
+        return false;
     }
 }
 ?>
---
+++ html/public/newsletter.php
@@ -0,0 +1,70 @@
+<?php
+session_start();
+require_once __DIR__ . "/../private/config.php";
+require_once __DIR__ . "/../private/includes/newsletter_functions.php";
+require_once __DIR__ . "/../private/includes/user_functions.php";
+
+if (isset($_SESSION['message'])) {
+    echo "<p>{$_SESSION['message']}</p>";
+    unset($_SESSION['message']);
+}
+
+$newsletter_id = $_GET['id'];
+$newsletter = get_newsletter($newsletter_id);
+
+if (!isset($_SESSION['user'])) {
+    $state = 'not_logged_in';
+    $user_email = null;
+    $user_id = null;
+} else {
+    $user_email = $_SESSION['user']['email'];
+    $user_id = get_user_id($user_email);
+
+    if (user_is_subscribed($user_id, $newsletter_id)) {
+        $state = 'subscribed';
+    } else {
+        $state = 'not_subscribed';
+    }
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $action = $_POST['action'] ?? '';
+
+    switch ($action) {
+        case 'subscribe':
+            subscribe_to_newsletter($user_id, $newsletter_id);
+            $_SESSION['message'] = "You are now subscribed to " . $newsletter['title'];
+            header("Location: newsletter.php?id=" . $newsletter_id);
+            break;
+        case 'unsubscribe':
+            unsubscribe_to_newsletter($user_id, $newsletter_id);
+            $_SESSION['message'] = "Your subscription to <strong> " . $newsletter['title'] . "</strong> has been cancelled!";
+            header("Location: newsletter.php?id=" . $newsletter_id);
+            break;
+    }
+}
+?>
+
+<body>
+    <main>
+        <h2><?php echo $newsletter['title']; ?></h2>
+        <p><?php echo $newsletter['description']; ?></h2>
+        <p><?php echo "Created by:" . $newsletter['user_email']; ?></p>
+        <p><?php echo $newsletter['created_at']; ?></p>
+
+        <form method="POST">
+            <?php switch ($state):
+                case 'not_logged_in':
+                    echo '<button><a href="/public/login.php" class="button-link">Log in to subscribe</a></button>';
+                    break;
+                case 'subscribed':
+                    echo '<button type="submit" name="action" value="unsubscribe" onclick="window.location.reload()">Do not want to be subscribed anymore?</button>';
+                    break;
+                case 'not_subscribed':
+                    echo '<button type="submit" name="action" value="subscribe">Subscribe</button>';
+                    break;
+            endswitch;
+            ?>
+        </form>
+    </main>
+</body>
